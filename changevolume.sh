96454e45419f397013e8e1b6201e84fb0cde1059
Milanxam <96538473+Milanxam@users.noreply.github.com>
2023-03-13 23:21:21 +0100

    Add script file

---
+++ changevolume.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#function send_notification() {
+#	volume=$(pamixer --get-volume)
+#	dunstify -$icon_name -a "changevolume" -u low -r "9993" -h int:value:"$volume" -i "volume-$1" "Volume: ${volume}%" -t 2000
+
+
+function send_notification {
+	volume=$(pamixer --get-volume)
+
+if [ "$volume" = "0" ]; then
+	icon_name="~/.local/bin/icons/audio-volume-muted-symbolic.svg"
+	dunstify -i ~/.local/bin/icons/audio-volume-muted-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
+    else
+	if [ "$volume" -lt "40" ]; then
+		icon_name="~/.local/bin/icons/audio-volume-low-symbolic.svg"
+		dunstify -i ~/.local/bin/icons/audio-volume-low-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
+	else
+	    if [ "$volume" -lt "70" ]; then
+		icon_name="~/.local/bin/icons/audio-volume-medium-symbolic.svg"
+		dunstify -i ~/.local/bin/icons/audio-volume-medium-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
+	    else
+		if [ "$volume" -lt "100" ]; then
+		    icon_name="~/.local/bin/icons/audio-volume-high-symbolic.svg"
+		    dunstify -i ~/.local/bin/icons/audio-volume-high-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
+		else
+		    icon_name="~/.local/bin/icons/audio-volume-overamplified-symbolic.svg"
+		     dunstify -i ~/.local/bin/icons/audio-volume-overamplified-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000		
+	    fi
+        fi
+    fi
+fi
+
+}
+
+
+case $1 in
+up)
+	# Set the volume on (if it was muted)
+	pamixer -u
+	pamixer -i 5 --allow-boost
+	send_notification $1
+	;;
+down)
+	pamixer -u
+	pamixer -d 5 --allow-boost
+	send_notification $1
+	;;
+mute)
+	pamixer -t
+	if $(pamixer --get-mute); then
+		icon_name="$DIR/icons/audio-volume-muted-symbolic.svg"
+		dunstify -i ~/.local/bin/icons/audio-volume-muted-symbolic.svg -a "changevolume" -t 2000 -r 9993 -u low "Muted"
+ 	else
+		send_notification up
+	fi
+	;;
+esac
