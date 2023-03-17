8a03bde566ec799109fb7ba2487fe4eb8c9705bd
Milanxam <96538473+Milanxam@users.noreply.github.com>
2023-03-17 13:59:30 +0100

    Adjust icon directory path

--- changevolume.sh
+++ changevolume.sh
@@ -5,16 +5,16 @@ function send_notification {
 	volume=$(pamixer --get-volume)
 
 if [ "$volume" = "0" ]; then
-	dunstify -i ~/.local/bin/icons/audio-volume-muted-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
+	dunstify -i $DIR/icons/audio-volume-muted-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
     else
 	if [ "$volume" -lt "40" ]; then
-		dunstify -i ~/.local/bin/icons/audio-volume-low-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
+		dunstify -i $DIR/icons/audio-volume-low-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
 	else
 	    if [ "$volume" -lt "70" ]; then
-		dunstify -i ~/.local/bin/icons/audio-volume-medium-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
+		dunstify -i $DIR/icons/audio-volume-medium-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
 	    else
 		if [ "$volume" -lt "100" ]; then
-		    dunstify -i ~/.local/bin/icons/audio-volume-high-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
+		    dunstify -i $DIR/icons/audio-volume-high-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000
 		else
 		     dunstify -i $DIR/icons/audio-volume-overamplified-symbolic.svg -a "changevolume" -u low -r "9993" -h int:value:"$volume" "Volume: ${volume}%" -t 2000		
 	    fi
